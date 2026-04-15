fb7910296003c4e018d8dd6daff1b21cc1dc56f3
Angie <popeian1@msu.edu>
2026-04-15 22:59:42 +0000

    changes made to authentication file

--- db/migrate/20260413133241_create_users.rb
+++ db/migrate/20260413133241_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration[8.1]
   def change
     create_table :users do |t|
       t.string :email_address, null: false
-      t.string :username, null: false
       t.string :password_digest, null: false
+      t.string :username, null: false
       t.integer :recipe_one_id
       t.integer :recipe_two_id
       t.integer :recipe_third_id
