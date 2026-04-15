d8561525a5d5ef54883bd6c2f63380a9db9604fd
Angie <popeian1@msu.edu>
2026-04-15 22:31:33 +0000

    just added the table to the authentication (test?)

--- db/migrate/20260413133241_create_users.rb
+++ db/migrate/20260413133241_create_users.rb
@@ -2,7 +2,11 @@ class CreateUsers < ActiveRecord::Migration[8.1]
   def change
     create_table :users do |t|
       t.string :email_address, null: false
+      t.string :username, null: false
       t.string :password_digest, null: false
+      t.integer :recipe_one_id
+      t.integer :recipe_two_id
+      t.integer :recipe_third_id
 
       t.timestamps
     end
