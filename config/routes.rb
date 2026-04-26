256da39f55331ed620b3a5338e3f8be0562fad80
Angie <popeian1@msu.edu>
2026-04-26 05:28:43 +0000

    links for sign up page and log in page work

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
   get "/user_info/about", to: "user_info#about"
   get "/user_info/disorder/:id", to: "user_info#disorder"
   get "/user_info/recipe/:id", to: "user_info#recipe"
+  get "/user_info/create-acc", to: "user_info#create-acc"
+  get "/user_info/sign-in", to: "user_info#sign-in"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
